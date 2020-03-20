38d2af2b4d70f316e6e5aee7a275a281055a9844
Moataz Nabil <45570191+moatazeldebsy@users.noreply.github.com>
2020-03-20 19:58:15 +0100

    Create run-emulator.sh

---
+++ scripts/run-emulator.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -eu
+
+# start android emulator
+START=`date +%s` > /dev/null
+
+echo no | $ANDROID_HOME/tools/android create avd --force -n test -t android-21 --abi default/armeabi-v7a
+$ANDROID_HOME/tools/android list avd
+$ANDROID_HOME/tools/emulator64-arm -avd test -no-window -no-boot-anim -no-audio -verbose &
+
+bootanim=""
+failcounter=0
+timeout_in_sec=360
+
+until [[ "$bootanim" =~ "stopped" ]]; do
+  bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
+  if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline"
+    || "$bootanim" =~ "running" ]]; then
+    let "failcounter += 1"
+    echo "Waiting for emulator to start"
+    if [[ $failcounter -gt timeout_in_sec ]]; then
+      echo "Timeout ($timeout_in_sec seconds) reached; failed to start emulator"
+      exit 1
+    fi
+  fi
+  sleep 1
+done
+
+echo "Emulator is ready"
+# Unlock the Lock Screen
+$ANDROID_HOME/platform-tools/adb shell input keyevent 82 &
+$ANDROID_HOME/platform-tools/adb shell input keyevent 4 &
+DURATION=$(( `date +%s` - START )) > /dev/null
+echo "Android Emulator started after $DURATION seconds."
+
+# emulator isn't ready yet, wait 1 min more
+# prevents APK installation error
+sleep 60
