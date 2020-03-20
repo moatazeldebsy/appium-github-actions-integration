f6bd3bd8276dc17a510c80b82c232ac90bed8a8a
Moataz Nabil <45570191+moatazeldebsy@users.noreply.github.com>
2020-03-20 23:49:41 +0100

    Update run-emulator.sh

--- scripts/run-emulator.sh
+++ scripts/run-emulator.sh
@@ -5,7 +5,7 @@ set -eu
 # start android emulator
 START=`date +%s` > /dev/null
 
-echo no | $ANDROID_HOME/tools/android create avd --force -n test -t android-21 --abi default/armeabi-v7a
+echo no | $ANDROID_HOME/tools/android create avd --force -n test --abi default/armeabi-v7a
 $ANDROID_HOME/tools/android list avd
 $ANDROID_HOME/tools/emulator64-arm -avd test -no-window -no-boot-anim -no-audio -verbose &
 
