fbdb35eb2bbc2b4890caddd0adceb5c82b786a2e
Moataz Nabil <45570191+moatazeldebsy@users.noreply.github.com>
2020-03-22 16:36:46 +0100

    Update run-emulator.sh

--- scripts/run-emulator.sh
+++ scripts/run-emulator.sh
@@ -3,3 +3,36 @@
 set -eu
 cd $ANDROID_HOME/tools/bin
 sdkmanager --list
+# start android emulator
+echo no | $ANDROID_HOME/tools/android create avd --force --name testAVD --abi google_apis/x86_64 --package 'system-images;android-10;default;armeabi-v7a '
+$ANDROID_HOME/tools/android list avd
+$ANDROID_HOME/tools/emulator64-arm -avd test -no-window -no-boot-anim -no-audio -verbose &
+
+bootanim=""
+failcounter=0
+timeout_in_sec=360
+
+until [[ "$bootanim" =~ "stopped" ]]; do
+  bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
+  if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline"
+    || "$bootanim" =~ "running" ]]; then
+    let "failcounter += 1"
+    echo "Waiting for emulator to start"
+    if [[ $failcounter -gt timeout_in_sec ]]; then
+      echo "Timeout ($timeout_in_sec seconds) reached; failed to start emulator"
+      exit 1
+    fi
+  fi
+  sleep 1
+done
+
+echo "Emulator is ready"
+# Unlock the Lock Screen
+$ANDROID_HOME/platform-tools/adb shell input keyevent 82 &
+$ANDROID_HOME/platform-tools/adb shell input keyevent 4 &
+DURATION=$(( `date +%s` - START )) > /dev/null
+echo "Android Emulator started after $DURATION seconds."
+
+# emulator isn't ready yet, wait 1 min more
+# prevents APK installation error
+sleep 60
