84c54bc5e44825a544324227f252e88b42aa4f98
Moataz Nabil <45570191+moatazeldebsy@users.noreply.github.com>
2022-11-04 22:49:23 +0200

    Create install-android-sdk.sh

---
+++ scripts/install-android-sdk.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -i
+#using shebang with -i to enable interactive mode (auto load .bashrc)
+
+set -e #stop immediately if any error happens
+
+# Install Open SDK
+apt update
+apt install openjdk-8-jdk -y
+update-java-alternatives --set java-1.8.0-openjdk-amd64
+java -version
+
+# Install SDK Manager
+# you can find this file at https://developer.android.com/studio/index.html#downloads - section command line only
+cd ~ && wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
+ANDROID_HOME=/opt/androidsdk
+mkdir -p $ANDROID_HOME
+apt install unzip -y && unzip sdk-tools-linux-4333796.zip -d $ANDROID_HOME
+
+echo "export ANDROID_HOME=$ANDROID_HOME" >> ~/.bashrc
+echo 'export SDK=$ANDROID_HOME' >> ~/.bashrc
+echo 'export PATH=$SDK/emulator:$SDK/tools:$SDK/tools/bin:$SDK/platform-tools:$PATH' >> ~/.bashrc
+source ~/.bashrc
+
+# Install Android Image version 28
+yes | sdkmanager "platform-tools" "platforms;android-28" "emulator"
+yes | sdkmanager "system-images;android-28;google_apis;x86_64"
+emulator -version
+
+echo "INSTALL ANDROID SDK DONE!"
+echo "run 01.emulator-up.sh [new device name] to start emulator"
