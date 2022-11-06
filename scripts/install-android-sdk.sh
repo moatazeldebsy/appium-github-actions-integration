2bdefd373097b74a3cd9dfba8b9196973799721f
Moataz Nabil <45570191+moatazeldebsy@users.noreply.github.com>
2022-11-06 14:23:15 +0200

    Update install-android-sdk.sh

--- scripts/install-android-sdk.sh
+++ scripts/install-android-sdk.sh
@@ -5,27 +5,39 @@ set -e #stop immediately if any error happens
 
 # Install Open SDK
 sudo apt update
-sudo apt install openjdk-8-jdk -y
-sudo update-java-alternatives --set java-1.8.0-openjdk-amd64
+sudo apt install openjdk-11-jdk -y
+sudo update-java-alternatives --set openjdk-11-jdk-amd64
 sudo apt-get install android-sdk
 
 java -version
 
-# Install SDK Manager
-# you can find this file at https://developer.android.com/studio/index.html#downloads - section command line only
-cd ~ && wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
-ANDROID_HOME=/opt/androidsdk
-mkdir -p $ANDROID_HOME
-sudo apt install unzip -y && unzip sdk-tools-linux-4333796.zip -d $ANDROID_HOME
+ls -l ~/Downloads
+sudo mkdir ~/DevTools
+sudo mkdir ~/DevTools/Android
+sudo mkdir ~/DevTools/Android/cmdline-tools
 
-echo "export ANDROID_HOME=$ANDROID_HOME" >> ~/.bashrc
-echo 'export SDK=$ANDROID_HOME' >> ~/.bashrc
-echo 'export PATH=$SDK/emulator:$SDK/tools:$SDK/tools/bin:$SDK/platform-tools:$PATH' >> ~/.bashrc
+wget commandlinetools-linux-8512546_latest.zip
+unzip commandlinetools-linux-8512546_latest.zip
+
+mv tools ~/DevTools/Android/cmdline-tools/
+ls -l ~/DevTools/Android/cmdline-tools/tools
+sudo nano ~/.bashrc
+
+DEV_TOOLS="/home/$USER/DevTools"
+JAVA_HOME="$DEV_TOOLS/JDK/jdk-11.0.7+10"
+ANDROID_HOME="$DEV_TOOLS/Android"
+export JAVA_HOME
+export ANDROID_HOME
+PATH="$JAVA_HOME/bin:$ANDROID_HOME/cmdline-tools/tools/bin:$ANDROID_HOME/platform-tools:$PATH"
+
 source ~/.bashrc
+sdkmanager --version
 
-# Install Android Image version 28
-yes | sudo sdkmanager "platform-tools" "platforms;android-28" "emulator"
-yes | sudo sdkmanager "system-images;android-28;google_apis;x86_64"
-emulator -version
+sdkmanager --list
+sdkmanager "platform-tools" "platforms;android-29"
+sdkmanager "build-tools" "build-tools;29.0.3"
+
+yes | sdkmanager --licenses
+sdkmanager --update
 
 echo "INSTALL ANDROID SDK DONE!"
