3d84fe8919399a4132f207618077cdd39cd615ea
Moataz Nabil <45570191+moatazeldebsy@users.noreply.github.com>
2022-11-06 21:22:09 +0200

    Update emulator-up.sh

--- scripts/emulator-up.sh
+++ scripts/emulator-up.sh
@@ -5,7 +5,7 @@
 set -e #stop immediately if any error happens
 
 echo $ANDROID_HOME
-echo no | $ANDROID_HOME/tools/bin/avdmanager create avd -n avd30 -k "system-images;android-30;google_apis;x86"
+echo no | $ANDROID_HOME/tools/bin/avdmanager create avd -n avd30 -k "system-images;android-32;google_apis_playstore;arm64-v8a"
 
 # start the emulator
 $ANDROID_HOME/tools/bin/emulator -avd avd30 -no-audio -no-window -no-accel &
