8c2fd90b9f2c6148840cc0fa26fcd2c74965808f
Moataz Nabil <45570191+moatazeldebsy@users.noreply.github.com>
2022-11-06 21:24:58 +0200

    Update emulator-up.sh

--- scripts/emulator-up.sh
+++ scripts/emulator-up.sh
@@ -4,11 +4,19 @@
 
 set -e #stop immediately if any error happens
 
-echo $ANDROID_HOME
-echo no | $ANDROID_HOME/tools/bin/avdmanager create avd -n avd30 -k "system-images;android-32;google_apis_playstore;arm64-v8a"
+sudo $ANDROID_HOME/tools/bin/sdkmanager --update
+$ANDROID_HOME/tools/bin/sdkmanager --install "emulator"
+$ANDROID_HOME/tools/bin/sdkmanager --install "system-images;android-29;google_apis;x86"
+echo "no" | $ANDROID_HOME/tools/bin/avdmanager --verbose create avd --force --name "pixel" --device "pixel" --package "system-images;android-29;google_apis;x86" --tag "google_apis" --abi "x86"
+$ANDROID_HOME/emulator/emulator-headless @pixel -no-boot-anim -netdelay none -no-snapshot -wipe-data -verbose -show-kernel -no-audio -gpu swiftshader_indirect -no-snapshot &> /tmp/log.txt &
+sleep 160
+
+#echo no | $ANDROID_HOME/tools/bin/avdmanager create avd -n avd30 -k "system-images;android-32;google_apis_playstore;arm64-v8a"
 
 # start the emulator
-$ANDROID_HOME/tools/bin/emulator -avd avd30 -no-audio -no-window -no-accel &
+#$ANDROID_HOME/tools/bin/emulator -avd avd30 -no-audio -no-window -no-accel &
 
 # show connected virtual device
 adb devices
+
+
