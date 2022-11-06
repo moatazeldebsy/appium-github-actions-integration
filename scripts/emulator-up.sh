9a092a41001f46db75907d6dd5c3ea780178ed31
Moataz Nabil <45570191+moatazeldebsy@users.noreply.github.com>
2022-11-06 21:04:16 +0200

    Update emulator-up.sh

--- scripts/emulator-up.sh
+++ scripts/emulator-up.sh
@@ -9,7 +9,7 @@ echo $ANDROID_HOME
 echo no | $ANDROID_HOME/tools/bin/avdmanager create avd -n avd29 -k "system-images;android-29;google_apis;x86_64"
 
 # start the emulator
-tools/bin/emulator -avd avd29 -no-audio -no-window -no-accel &
+$ANDROID_HOME/tools/bin/emulator -avd avd29 -no-audio -no-window -no-accel &
 
 # show connected virtual device
 adb devices
