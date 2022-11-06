e148a5b55b4c5d928ff51be2d259890a4dcbcba3
Moataz Nabil <45570191+moatazeldebsy@users.noreply.github.com>
2022-11-06 21:03:07 +0200

    Update emulator-up.sh

--- scripts/emulator-up.sh
+++ scripts/emulator-up.sh
@@ -4,20 +4,12 @@
 
 set -e #stop immediately if any error happens
 
-avd_name=$1
-
-if [[ -z "$avd_name" ]]; then
-  avd_name="avd29"
-fi
-
-#check if emulator work well
-emulator -version
-
 # create virtual device, default using Android 10 Pie image (API Level 29)
-echo no | avdmanager create avd -n avd29 -k "system-images;android-29;google_apis;x86_64"
+echo $ANDROID_HOME
+echo no | $ANDROID_HOME/tools/bin/avdmanager create avd -n avd29 -k "system-images;android-29;google_apis;x86_64"
 
 # start the emulator
-emulator -avd avd29 -no-audio -no-window -no-accel &
+tools/bin/emulator -avd avd29 -no-audio -no-window -no-accel &
 
 # show connected virtual device
 adb devices
