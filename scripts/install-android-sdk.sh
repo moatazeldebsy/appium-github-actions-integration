70dd37acca4df2bcc65b9e7776ef749e4981ff2d
Moataz Nabil <45570191+moatazeldebsy@users.noreply.github.com>
2022-11-06 19:16:24 +0200

    Update install-android-sdk.sh

--- scripts/install-android-sdk.sh
+++ scripts/install-android-sdk.sh
@@ -3,40 +3,41 @@
 
 set -e #stop immediately if any error happens
 
-# Install Open SDK
 sudo apt update
-sudo apt-get install openjdk-11-jdk -y
+sudo apt install default-jre
+sudo apt install default-jdk
+
 sudo apt-get install android-sdk
 
 java -version
+javac -version
+
+sudo apt update
+
+
 
 ls -l ~/Downloads
 sudo mkdir ~/DevTools
 sudo mkdir ~/DevTools/Android
 sudo mkdir ~/DevTools/Android/cmdline-tools
 
-wget commandlinetools-linux-8512546_latest.zip
-unzip commandlinetools-linux-8512546_latest.zip
+# Install SDK Manager
+cd ~ && wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
 
-mv tools ~/DevTools/Android/cmdline-tools/
-ls -l ~/DevTools/Android/cmdline-tools/tools
-sudo nano ~/.bashrc
 
-echo DEV_TOOLS="/home/$USER/DevTools"
-echo JAVA_HOME="$DEV_TOOLS/JDK/jdk-11.0.7+10"
-echo ANDROID_HOME="$DEV_TOOLS/Android"
-echo export JAVA_HOME
-echo export ANDROID_HOME
-echo PATH="$JAVA_HOME/bin:$ANDROID_HOME/cmdline-tools/tools/bin:$ANDROID_HOME/platform-tools:$PATH"
 
+ANDROID_HOME=/opt/androidsdk
+mkdir -p $ANDROID_HOME
+sudo apt install unzip -y && unzip sdk-tools-linux-4333796.zip -d $ANDROID_HOME
+
+echo "export ANDROID_HOME=$ANDROID_HOME" >> ~/.bashrc
+echo 'export SDK=$ANDROID_HOME' >> ~/.bashrc
+echo 'export PATH=$SDK/emulator:$SDK/tools:$SDK/tools/bin:$SDK/platform-tools:$PATH' >> ~/.bashrc
 source ~/.bashrc
-sdkmanager --version
 
-sdkmanager --list
-sdkmanager "platform-tools" "platforms;android-29"
-sdkmanager "build-tools" "build-tools;29.0.3"
-
-yes | sdkmanager --licenses
-sdkmanager --update
+# Install Android Image version 28
+yes | sudo sdkmanager "platform-tools" "platforms;android-28" "emulator"
+yes | sudo sdkmanager "system-images;android-28;google_apis;x86_64"
+emulator -version
 
 echo "INSTALL ANDROID SDK DONE!"
